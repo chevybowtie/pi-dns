4c03351163d94d51320f4270e9a1e0137a7f1b9b
Simon Painter <simon@Simons-MacBook.broadband>
2019-09-29 17:08:34 +0100

    Made the widget work

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
-function create(name) {
-  $.get( "https://api.pi-dns.me/v1/stable/create", { name: name} , function( data ) {
+function create() {
+  subdomain = $("#subdomain").val();
+  $.get( "https://api.pi-dns.me/v1/stable/create", { name: subdomain} , function( data ) {
     alert( "Status Code: " + data.statusCode + "\n" +
             "Response: " + data.response
             );
