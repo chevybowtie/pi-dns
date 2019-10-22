1177866d434d0a8a0883ccdee3e5c4dda1812124
Simon Painter <simon@Simons-MacBook.broadband>
2019-10-22 13:56:06 +0100

    Added new widget

--- www/library.js
+++ www/library.js
@@ -6,3 +6,12 @@ function create() {
             );
   });
 }
+function update() {
+  secret = $("#secret").val();
+  name = $("#name").val();
+  $.get( "https://api.pi-dns.me/v1/stable/update", { name: name, secret: secret} , function( data ) {
+    alert( "Status Code: " + data.statusCode + "\n" +
+            "Response: " + data.response
+            );
+  });
+}
